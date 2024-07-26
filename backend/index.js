08cb029435cbbba36d170a467cc6a06a4b48d39d
Shivraj <shivkolekar01@gmail.com>
2024-07-26 16:46:02 +0530

    added frontend url

--- backend/index.js
+++ backend/index.js
@@ -14,7 +14,7 @@ app.use('/uploads', express.static(path.join(__dirname, 'uploads')))
 
 app.use(
   cors({
-    origin: 'http://localhost:5173',
+    origin: 'https://dev-blogs-seven-delta.vercel.app/',
     methods: ['GET', 'POST'],
     allowedHeaders: ['Content-Type']
   })
