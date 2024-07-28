8fab5202a3728346185978187f3a8128ffad5335
Shivraj <shivkolekar01@gmail.com>
2024-07-29 01:01:57 +0530

    updated frontend

--- frontend/src/components/BlogCard.jsx
+++ frontend/src/components/BlogCard.jsx
@@ -6,22 +6,24 @@ function BlogCard (props) {
   const { thumbnail, date, title, author, description } = props
   return (
     <>
-      <div className='BlogCard grid grid-rows-12 mx-2 h-auto cursor-pointer  bg-slate-900 my-2 sm:col-span-2 md:col-span-3 lg:col-span-4 border-gray-600 shadow-slate-950 drop-shadow-lg hover:bg-slate-800 hover:scale-105 hover:shadow-lg transition-transform transform border-2  text-white rounded-2xl   '>
-        <div className='grid row-span-6 p-2 '>
+      <div className='BlogCard px-4 grid pt-4 grid-rows-12 mx-2 h-fit cursor-pointer  bg-zinc-900 my-2 sm:col-span-2 md:col-span-3 lg:col-span-4 border-gray-700 shadow-slate-950 drop-shadow-lg hover:border-sky-300  hover:scale-105 hover:shadow-lg transition-transform transform border-2 text-white rounded-xl   '>
+        <div className='grid row-span-6   '>
           <img
-            className=' image rounded-xl h-full w-full'
+            className=' image rounded-lg h-full w-full'
             src={`${import.meta.env.VITE_API_URL}/${thumbnail}`}
             alt='Thumbnail'
           />
         </div>
-        <div className='p-4  grid row-span-5'>
-          <p className=' text-sm'>{props.date}</p>
-          <h1 className='text-white  text-xl flex flex-wrap'>{props.title}</h1>
-          <p className=''>By: {props.author}</p>
-          <p className=''>{props.description}</p>
+        <div className='  grid row-span-5'>
+          <p className=' text-sm mb-2 font-medium'>{props.date}</p>
+          <h1 className='text-white mb-2 font-semibold text-2xl flex flex-wrap'>
+            {props.title}
+          </h1>
+          <p className='text-lg mb-4 font-medium'>By: {props.author}</p>
+          {/*<p className='text-xl mb-2 font-semibold'>{props.description}</p>*/}
           <Link
             to={`/blog/${props.title}`}
-            className='bg-sky-900 flex align-bottom text-white rounded-xl p-2 hover:bg-sky-800 hover:scale-105 hover:shadow-lg transition-transform transform'
+            className='bg-white text-zinc-900 mb-0 py-2 mr-9 px-2 flex justify-center align-middle font-bold text-xl rounded-lg hover:scale-105 hover:shadow-lg transition-transform transform border-2   '
           >
             Read More
           </Link>
